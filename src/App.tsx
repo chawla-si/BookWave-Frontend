5f18ccf7ee1b96c12dbca547fb84c3302f971f41
chawla-si <chawla.si@northeastern.edu>
2023-11-18 17:42:47 -0500

    Add Book Crousel static component

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 
 import './App.css';
 import { ExploreTopBooks } from './layouts/Homepage/ExploreTopBooks';
+import { Carousel } from './layouts/Homepage/Carousel';
 
 function App() {
   return (
     <div>
       <Navbar />
       <ExploreTopBooks />
+      <Carousel />
     </div>
   );
 }
