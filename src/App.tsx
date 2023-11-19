94013d5713b4f9605202d0ff7381419a55d682a1
sijoyalmeida09 <144766943+sijoyalmeida09@users.noreply.github.com>
2023-11-18 21:01:17 -0500

    Add LibraryServices to ask user cant find what you looking for

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
 import React from 'react';
-import { Navbar } from './layouts/NavbarAndFooter/Navbar';
-
 import './App.css';
+
+import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 import { ExploreTopBooks } from './layouts/Homepage/ExploreTopBooks';
 import { Carousel } from './layouts/Homepage/Carousel';
 import { Heros } from './layouts/Homepage/Heros';
+import { LibraryServices } from './layouts/Homepage/LibraryServices';
 
 function App() {
   return (
     <div>
       <Navbar />
       <ExploreTopBooks />
-      <Carousel />
+      <Carousel/>
       <Heros/>
+      <LibraryServices/>
     </div>
   );
 }
