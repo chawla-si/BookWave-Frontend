dddbc131cd5f394550d74535667be108504d519e
chawla-si <chawla.si@northeastern.edu>
2023-11-21 11:04:32 -0500

    Add SearchBooks in SearchBookComponent

--- src/layouts/SearchBooksPage/SearchBooksPage.tsx
+++ src/layouts/SearchBooksPage/SearchBooksPage.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import BookModel from '../../models/BookModel';
-import { Pagination } from '../Utils/Pagination';
+import { SearchBook } from './components/SearchBook';
 import { SpinnerLoading } from '../Utils/SpinnerLoading';
 import { directive } from '@babel/types';
 
@@ -86,9 +86,9 @@ export const SearchBooksPage = () => {
 
     const categoryField = (value: string) => {
         if (
-            value.toLowerCase() === 'fe' || 
-            value.toLowerCase() === 'be' || 
-            value.toLowerCase() === 'data' || 
+            value.toLowerCase() === 'fe' ||
+            value.toLowerCase() === 'be' ||
+            value.toLowerCase() === 'data' ||
             value.toLowerCase() === 'devops'
         ) {
             setCategorySelection(value);
@@ -108,7 +108,81 @@ export const SearchBooksPage = () => {
 
     return (
         <div>
-            
+            <div className='container'>
+                <div>
+                    <div className='row mt-5'>
+                        <div className='col-6'>
+                            <div className='d-flex'>
+                                <input className='form-control me-2' type='search'
+                                    placeholder='Search' aria-labelledby='Search'
+                                    onChange={e => setSearch(e.target.value)} />
+                                <button className='btn btn-outline-success'
+                                    onClick={() => searchHandleChange()}>
+                                    Search
+                                </button>
+                            </div>
+                        </div>
+                        <div className='col-4'>
+                            <div className='dropdown'>
+                                <button className='btn btn-secondary dropdown-toggle' type='button'
+                                    id='dropdownMenuButton1' data-bs-toggle='dropdown'
+                                    aria-expanded='false'>
+                                    Category
+                                </button>
+                                <ul className='dropdown-menu' aria-labelledby='dropdownMenuButton1'>
+                                    <li onClick={() => categoryField('All')}>
+                                        <a className='dropdown-item' href='#'>
+                                            All
+                                        </a>
+                                    </li>
+                                    <li onClick={() => categoryField('FE')}>
+                                        <a className='dropdown-item' href='#'>
+                                            Front End
+                                        </a>
+                                    </li>
+                                    <li onClick={() => categoryField('BE')}>
+                                        <a className='dropdown-item' href='#'>
+                                            Back End
+                                        </a>
+                                    </li>
+                                    <li onClick={() => categoryField('Data')}>
+                                        <a className='dropdown-item' href='#'>
+                                            Data
+                                        </a>
+                                    </li>
+                                    <li onClick={() => categoryField('DevOps')}>
+                                        <a className='dropdown-item' href='#'>
+                                            DevOps
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    {totalAmountOfBooks > 0 ?
+                        <>
+                            <div className='mt-3'>
+                                <h5>Number of results: (22)</h5>
+                            </div>
+                            <p>
+                                1 to 5 of 22 items:
+                            </p>
+                            {books.map(book => (
+                                <SearchBook book={book} key={book.id} />
+                            ))}
+                        </>
+                        :
+                        <div className='m-5'>
+                            <h3>
+                                Can't find what you are looking for?
+                            </h3>
+                            <a type='button' className='btn main-color btn-md px-4 me-md-2 fw-bold text-white'
+                                href='#'>Library Services</a>
+                        </div>
+                    }
+                    
+                </div>
+            </div>
         </div>
     );
 }
